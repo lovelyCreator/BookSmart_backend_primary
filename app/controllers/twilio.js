fce9fc0bbabce7c5e1f6fcd42a6f7da8dc4892b8
LovelyCreator <lovely7rh@gmail.com>
2024-08-17 20:06:10 +1000

    initail commit

---
+++ app/controllers/twilio.js
@@ -0,0 +1,42 @@
+const twilio = require('twilio');
+const dotenv = require('dotenv').config();
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+exports.createVerification = async (verifyPhone) => {
+    console.log('verification');
+    const verification = await client.verify.v2
+        .services(process.env.TWILIO_SERVICE_SID)
+        .verifications.create({
+            channel: "sms",
+            to: verifyPhone,
+        });
+    console.log(verification.status);
+    return verification.status;
+}
+
+exports.createVerificationCheck = async function (to, code) {
+    const verificationCheck = await client.verify.v2
+        .services(process.env.TWILIO_SERVICE_SID)
+        .verificationChecks.create({
+            code: code,
+            to: to,
+        });
+    console.log(verificationCheck.status);
+    return verificationCheck.status;
+}
+
+exports.pushNotification = async function (message, address) {
+    const notificationOpts = {
+        toBinding: JSON.stringify({
+            binding_type: 'sms',
+            address: address,
+        }),
+        body: message,
+    };
+    client.notify.v1
+        .services(process.env.TWILIO_NOTIFY_SID)
+        .notifications.create(notificationOpts)
+        .then(notification => console.log(notification.sid))
+        .catch(error => console.log(error));
+}
